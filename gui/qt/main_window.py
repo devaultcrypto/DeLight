8b2bdb6b16d4e4df5fdce386d31829f171e5c956
jon <jonaldfyookball@outlook.com>
2017-08-18 00:00:38 -0400

    Remove dynamic fees.

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -2404,11 +2404,11 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
             self.config.set_key('dynamic_fees', x == Qt.Checked)
             self.fee_slider.update()
             update_maxfee()
-        dynfee_cb = QCheckBox(_('Use dynamic fees'))
-        dynfee_cb.setChecked(self.config.is_dynfee())
-        dynfee_cb.setToolTip(_("Use fees recommended by the server."))
-        fee_widgets.append((dynfee_cb, None))
-        dynfee_cb.stateChanged.connect(on_dynfee)
+        #dynfee_cb = QCheckBox(_('Use dynamic fees'))
+        #dynfee_cb.setChecked(self.config.is_dynfee())
+        #dynfee_cb.setToolTip(_("Use fees recommended by the server."))
+        #fee_widgets.append((dynfee_cb, None))
+        #dynfee_cb.stateChanged.connect(on_dynfee)
 
         def on_maxfee(x):
             m = maxfee_e.get_amount()
