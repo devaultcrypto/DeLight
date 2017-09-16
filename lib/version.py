18ad68760aad45efc09819fa63b66149388f1b50
Neil Booth <kyuupichan@gmail.com>
2017-09-16 15:01:17 +0900

    Remove remnants of 2FA and TrustedCoin support

--- lib/version.py
+++ lib/version.py
@@ -4,7 +4,6 @@ PROTOCOL_VERSION = '0.10'   # protocol version requested
 # The hash of the mnemonic seed must begin with this
 SEED_PREFIX      = '01'      # Electrum standard wallet
 SEED_PREFIX_SW   = '02'      # Electrum segwit wallet
-SEED_PREFIX_2FA  = '101'     # extended seed for two-factor authentication
 
 
 def seed_prefix(seed_type):
@@ -12,5 +11,3 @@ def seed_prefix(seed_type):
         return SEED_PREFIX
     elif seed_type == 'segwit':
         return SEED_PREFIX_SW
-    elif seed_type == '2fa':
-        return SEED_PREFIX_2FA
