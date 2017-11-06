7727dd6a957423a72070656bc8f5ac36cd26bfba
Daniel <daniel@dconnolly.com>
2017-11-06 22:14:44 +0100

    change indents to spaces

--- lib/blockchain.py
+++ lib/blockchain.py
@@ -325,22 +325,21 @@ class Blockchain(util.PrintError):
         return sorted(times)[len(times) // 2]
 
     def get_suitable_block_height(self, suitableheight):
+        #In order to avoid a block in a very skewed timestamp to have too much
+        #influence, we select the median of the 3 top most block as a start point
+        #Reference: github.com/Bitcoin-ABC/bitcoin-abc/master/src/pow.cpp#L201
+        blocks2 = self.read_header(suitableheight)
+        blocks1 = self.read_header(suitableheight-1)
+        blocks = self.read_header(suitableheight-2)
 
-	#In order to avoid a block in a very skewed timestamp to have too much
-	#influence, we select the median of the 3 top most block as a start point
-	#Reference: github.com/Bitcoin-ABC/bitcoin-abc/master/src/pow.cpp#L201
-	blocks2 = self.read_header(suitableheight)
-	blocks1 = self.read_header(suitableheight-1)
-	blocks = self.read_header(suitableheight-2)
-	
-	if (blocks['timestamp'] > blocks2['timestamp'] ):
-		blocks,blocks2 = blocks2,blocks
-	if (blocks['timestamp'] > blocks1['timestamp'] ):
-		blocks,blocks1 = blocks1,blocks
-	if (blocks1['timestamp'] > blocks2['timestamp'] ):
-		blocks1,blocks2 = blocks2,blocks1
-	
-	return blocks1['block_height']
+        if (blocks['timestamp'] > blocks2['timestamp'] ):
+            blocks,blocks2 = blocks2,blocks
+        if (blocks['timestamp'] > blocks1['timestamp'] ):
+            blocks,blocks1 = blocks1,blocks
+        if (blocks1['timestamp'] > blocks2['timestamp'] ):
+            blocks1,blocks2 = blocks2,blocks1
+
+        return blocks1['block_height']
 
     def get_bits(self, header):
         '''Return bits for the given height.'''
@@ -361,16 +360,16 @@ class Blockchain(util.PrintError):
                 return MAX_BITS
 
 
-	#NOV 13 HF DAA
-	
-	prevheight = height -1
+        #NOV 13 HF DAA
+
+        prevheight = height -1
         daa_mtp=self.get_median_time_past(prevheight)
-	
-	#######FOR TESTING        
-	print ("debug, mtp is ",daa_mtp)
-	print ("VALIDATING BLOCK HEIGHT ",height)
+
+        #######FOR TESTING
+        print ("debug, mtp is ",daa_mtp)
+        print ("VALIDATING BLOCK HEIGHT ",height)
         if (daa_mtp >= 1509559291):  #leave this here for testing
-        #if (daa_mtp >= 1510600000):
+            #if (daa_mtp >= 1510600000):
             daa_starting_height=self.get_suitable_block_height(prevheight-144)
             daa_ending_height=self.get_suitable_block_height(prevheight)
             daa_cumulative_work=0
@@ -386,22 +385,22 @@ class Blockchain(util.PrintError):
                     daa_bits_for_a_block=daa_prior['bits']
                     daa_work_for_a_block=bits_to_work(daa_bits_for_a_block)
                     daa_cumulative_work += daa_work_for_a_block
-	    daa_elapsed_time=daa_ending_timestamp-daa_starting_timestamp
-	    if (daa_elapsed_time>172800):
+            daa_elapsed_time=daa_ending_timestamp-daa_starting_timestamp
+            if (daa_elapsed_time>172800):
                 daa_elapsed_time=172800
             if (daa_elapsed_time<43200):
                 daa_elapsed_time=43200
             daa_Wn= (daa_cumulative_work*600)//daa_elapsed_time
-	    daa_target= (1 << 256) // daa_Wn -1
-	    daa_retval = target_to_bits(daa_target)
-	    daa_retval = int(daa_retval)
+            daa_target= (1 << 256) // daa_Wn -1
+            daa_retval = target_to_bits(daa_target)
+            daa_retval = int(daa_retval)
             #####FOR TESTING
-	    print ("at height ",height)
-	    print ("daa_Wn ",daa_Wn)
-	    print ("daa_retval is ",daa_retval)
+            print ("at height ",height)
+            print ("daa_Wn ",daa_Wn)
+            print ("daa_retval is ",daa_retval)
             return daa_retval
 
-         #END OF NOV-2017 DAA
+            #END OF NOV-2017 DAA
 
         if height % 2016 == 0:
             return self.get_new_bits(height)
