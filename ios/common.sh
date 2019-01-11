4add9366500645a94a34efb8bc5623239f1d9355
Calin Culianu <calin.culianu@gmail.com>
2019-01-11 23:11:54 +0200

    [iOS] Rename of ElectronCash -> ElectronCashSLP  (#34)
    * [iOS] Rename of ElectronCash -> ElectronCashSLP
    
    This is so that the app built will be able to be installed side-by-side
    with the existing Electron Cash on iOS devices.
    
    * Updated readme.rst to match ec main
    
    * Pet peeve: made version for package come from lib/version.py
    
    This always bugged me. make_ios_project.sh has been fixed now.
    
    * A small nit.
    
    * 1 more nit
    
    * Fixup to appdelegate.py issue
    
    * updated common.sh with a caveat
    
    * rename application folder to 'ElectronCash'

---
+++ ios/common.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# The below are used by the shell scripts as well as setup.py to set things up.
+# This is the place to rename the target.app, etc.
+#
+# Caveat: Make sure compact_name matches the code directory in this folder!
+# Caveat2: Make sure overrides/ElectronCashSLP gets renamed to match compact_name!
+
+compact_name="ElectronCash"
+xcode_target="Electron-Cash-SLP"
