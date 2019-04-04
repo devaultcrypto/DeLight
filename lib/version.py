aa50955d0f3b6bd857f368037a4677290faa3f46
James Cramer <jcramer@simpleledger.io>
2019-04-04 09:25:31 -0400

    release versioning updates

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-PACKAGE_VERSION = '3.4.6'     # version of the client package
+PACKAGE_VERSION = '3.4.7'     # version of the client package
 PROTOCOL_VERSION = '1.4'     # protocol version requested
 
 # The hash of the mnemonic seed must begin with this
