65faf54fabf1ddb92abecc38b71713696ebc9266
Pro X <pro@proteanx.com>
2019-07-20 21:21:20 -0400

    update datadir

--- lib/util.py
+++ lib/util.py
@@ -451,7 +451,7 @@ def user_dir(prefer_local=False):
     if 'ANDROID_DATA' in os.environ:
         return android_data_dir()
     elif os.name == 'posix' and "HOME" in os.environ:
-        return os.path.join(os.environ["HOME"], ".electron-cash" )
+        return os.path.join(os.environ["HOME"], ".electron-dvt" )
     elif "APPDATA" in os.environ or "LOCALAPPDATA" in os.environ:
         app_dir = os.environ.get("APPDATA")
         localapp_dir = os.environ.get("LOCALAPPDATA")
