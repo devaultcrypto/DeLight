964be07dce5dc184e5295b19d77955155c2b33d2
Proteus <pro@proteanx.com>
2024-02-16 14:52:42 -0500

    use babel for generate strings + new output dir and refactor
    also ignore plurals error for 3 results instead of 2, rework this when there's time

--- android/app/generate_strings.py
+++ android/app/generate_strings.py
@@ -3,21 +3,37 @@
 # Generates strings.xml files from the gettext files. This script is run automatically by the
 # Gradle task `generateStrings`.
 #
-# This script requires the package `polib`. It also runs contrib/make_locale, which requires
-# the package `requests`, and the external commands `xgettext` and `msgfmt`.
+# This script's requirements are listed in build-requirements.txt. It also runs
+# contrib/make_locale, which requires the external commands `xgettext` and `msgfmt`.
+
+# Strings to test when changing this script:
+#
+# * String with no fields, e.g. "New wallet"
+# * % syntax:
+#   * Single implicit field, e.g. "%s bytes"
+#   * Single indexed field (no examples in catalog)
+#   * Multiple implicit fields (no examples in catalog)
+#   * Multiple indexed fields, e.g. "%1$d tx (%2$d unverified)"
+#   * Plurals, e.g. "Sweep %d input(s)"
+# * {} syntax:
+#   * Single implicit field, e.g. "{} copied to clipboard"
+#   * Single keyword field, e.g. "Size: {size} bytes" (no examples on Android, except for
+#     the plural below)
+#   * Multiple implicit fields, e.g. "{} contacts exported to '{}'" (no examples on Android)
+#   * Multiple keyword fields, e.g. "Please wait... {num}/{total}" (no examples on Android)
+#   * Plurals, e.g. "{conf} confirmation(s)"
+
 
 import argparse
+import babel
 from collections import Counter, defaultdict
 from datetime import datetime
 import os
 from os.path import abspath, basename, dirname, isdir, join
+import polib
 import re
 from subprocess import run
 import sys
-
-# We use polib because we need msgid_plural, which the standard library gettext module discards
-# during parsing of .mo files.
-import polib
 
 
 SCRIPT_NAME = basename(__file__)
@@ -25,12 +41,12 @@ EC_ROOT = abspath(join(dirname(__file__), "../.."))
 
 
 JAVA_KEYWORDS = set([
-    "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class",
-    "const", "continue", "default", "do", "double ", "else", "enum", "extends", "final",
+    "_", "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class",
+    "const", "continue", "default", "do", "double ", "else", "enum", "extends", "false", "final",
     "finally", "float", "for", "goto", "if", "implements", "import", "instanceof", "int",
-    "interface ", "long", "native", "new", "package", "private", "protected", "public",
+    "interface ", "long", "native", "new", "null", "package", "private", "protected", "public",
     "return", "short", "static", "strictfp", "super", "switch ", "synchronized", "this",
-    "throw", "throws", "transient", "try", "void", "volatile", "while"])
+    "throw", "throws", "transient", "true", "try", "void", "volatile", "while"])
 
 KOTLIN_KEYWORDS = set([  # "Hard" keywords only.
     "as", "break", "class", "continue", "do", "else", "false", "for", "fun", "if", "in",
@@ -39,115 +55,210 @@ KOTLIN_KEYWORDS = set([  # "Hard" keywords only.
 
 KEYWORDS = JAVA_KEYWORDS | KOTLIN_KEYWORDS
 
-
-# Map from gettext plural formula to Android quantity keywords. This will have to be extended
-# as plural translations are added in more languages. Android uses the CLDR rules from
-# http://www.unicode.org/cldr/charts/25/supplemental/language_plural_rules.html.
-QUANTITIES = {
-    # e.g. zh
-    "0": {
-        0: "other"
-    },
-
-    # e.g. de
-    "(n != 1)": {
-        0: "one",
-        1: "other"
-    },
-
-    # e.g. fr
-    "(n > 1)": {
-        0: "one",   # For these languages, "one" includes zero (see CLDR link above).
-        1: "other"
-    },
-
-    # e.g. ro
-    "(n==1 ? 0 : (n==0 || (n%100>0 && n%100<20)) ? 1 : 2)": {
-        0: "one",
-        1: "few",
-        2: "other"
-    },
+# Some language codes have been renamed, but Android only recognizes the old codes in resource
+# directory names.
+RENAMED_LANGUAGES = {
+    "he": "iw",
+    "yi": "ji",
+    "id": "in",
 }
 
 
+catalog_errors = 0
+
+class CatalogError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        global catalog_errors
+        catalog_errors += 1
+
+
 def main():
+    global args
     args = parse_args()
     if not args.no_download:
         log("Running make_locale")
         run([sys.executable, join(EC_ROOT, "contrib/make_locale")], check=True)
 
     locale_dir = join(EC_ROOT, "lib/locale")
+    src_strings = read_catalog(join(locale_dir, "messages.pot"), "en", "US")
+
     lang_strings = defaultdict(list)
     for lang_region in [name for name in os.listdir(locale_dir)
                         if isdir(join(locale_dir, name)) and name != '__pycache__']:
         lang, region = lang_region.split("_")
-        catalog = read_catalog(join(locale_dir, lang_region, "LC_MESSAGES",
-                                    "electron-cash.mo"))
+        catalog = read_catalog(join(locale_dir, lang_region, "LC_MESSAGES", "electron-cash.po"),
+                               lang, region)
         lang_strings[lang].append((region, catalog))
 
-    src_strings = read_catalog(join(locale_dir, "messages.pot"))
+    if catalog_errors:
+        sys.exit(1)
+
+    log(f"Writing to {args.out}")
     ids = make_ids(src_strings)
+    write_xml(args.out, "", src_strings, ids)
     for lang, region_strings in lang_strings.items():
         region_strings.sort(key=region_order, reverse=True)
         for i, (region, strings) in enumerate(region_strings):
-            write_xml(lang if i == 0 else "{}-r{}".format(lang, region),
+            write_xml(args.out, lang if i == 0 else "{}-r{}".format(lang, region),
                       strings, ids)
 
-    # The main strings.xml should be generated last, because this script will only be
-    # automatically run if it's missing.
-    write_xml("", src_strings, ids)
 
+def read_catalog(filename, lang, region):
+    lang_region = f"{lang}_{region}"
 
-def read_catalog(filename):
     try:
         is_pot = filename.endswith(".pot")
-        f = (polib.mofile if filename.endswith(".mo") else polib.pofile)(filename)
+        f = polib.pofile(filename)
         pf = f.metadata.get("Plural-Forms")
         if pf is None:
             quantities = None
+        elif is_pot:
+            quantities = ["one", "other"]
         else:
-            match = re.search(r"plural=(.+?);", pf)
+            match = re.search(r"nplurals=(\d+);", pf)
             if not match:
-                raise Exception("Failed to parse Plural-Forms")
-            formula = match.group(1)
-            quantities = QUANTITIES.get(formula)
+                raise CatalogError("Failed to parse Plural-Forms")
+            nplurals = int(match.group(1))
 
-        catalog = {}
-        for entry in f:
             try:
-                msgid = entry.msgid
-                if is_excluded(msgid):
+                locale = babel.Locale(lang_region)
+            except babel.UnknownLocaleError:
+                locale = babel.Locale(lang)
+
+            quantities = sorted(locale.plural_form.tags | {"other"},
+                                key=["zero", "one", "two", "few", "many", "other"].index)
+         #   if len(quantities) != nplurals:
+          #      raise CatalogError(f"Plural-Forms says {nplurals=}, but Babel has "
+           #                        f"{len(quantities)} plural tags for this language {quantities}")
+    except CatalogError as e:
+        log(f"{filename}: {e}", file=sys.stderr)
+        return None
+
+    catalog = {}
+    for entry in f:
+        try:
+            msgid = entry.msgid
+            if is_excluded(msgid):
+                continue
+
+            if entry.msgid_plural:
+                # Get field indices from msgid_plural, because sometimes msgid just contains a
+                # singular word with no field.
+                indices = get_indices(entry.msgid_plural)
+
+                # Only include the string if it has a complete set of plural translations,
+                # otherwise some numbers would cause it to appear blank.
+                msgstr_plural = ({0: msgid, 1: entry.msgid_plural} if is_pot
+                                 else entry.msgstr_plural)
+                if not all(s for s in msgstr_plural.values()):
                     continue
 
-                # Replace Python str.format syntax with Java String.format syntax.
-                keywords = re.findall(r"\{(\w+)\}", msgid)
-                def fix_format(s):
-                    s = s.replace("{}", "%s")
-                    for k in keywords:
-                        s = s.replace("{" + k + "}",
-                                      "%{}$s".format(keywords.index(k) + 1))
-                    return s
+                if quantities is None:
+                    raise CatalogError("msgid {msgid!r} has plurals, but file has no Plural-Forms")
+                catalog[msgid] = {quantities[i]: fix_format(s, indices)
+                                  for i, s in msgstr_plural.items()}
+            else:
+                indices = get_indices(msgid)
+                msgstr = msgid if is_pot else entry.msgstr
+                if not msgstr:
+                    continue
+                catalog[msgid] = fix_format(msgstr, indices)
+        except CatalogError as e:
+            log(f"{filename}:{entry.linenum}: {e}", file=sys.stderr)
 
-                msgid = fix_format(msgid)
-                if entry.msgid_plural:
-                    if is_pot:
-                        catalog[msgid] = {"one": msgid,
-                                          "other": fix_format(entry.msgid_plural)}
-                    else:
-                        if quantities is None:
-                            raise Exception(
-                                "Unknown plural formula '{}': add it to QUANTITIES in {}"
-                                .format(formula, SCRIPT_NAME))
-                        catalog[msgid] = {quantities[i]: fix_format(s)
-                                          for i, s in entry.msgstr_plural.items()}
-                else:
-                    catalog[msgid] = msgid if is_pot else fix_format(entry.msgstr)
-            except Exception:
-                raise Exception("Failed to process entry '{}'".format(entry.msgid))
-        return catalog
+    return catalog
 
-    except Exception:
-        raise Exception("Failed to process '{}'".format(filename))
+
+# Takes a source string in {} or % format, and returns a dict mapping its field keywords and
+# indices (explicit or implicit) to % field indices to use in the output.
+def get_indices(s):
+    indices = {}
+
+    # {} field indices are assigned in the order each field first appears in the string.
+    matches = get_brace_fields(s)
+    if matches:
+        for _, index in matches:
+            if index not in indices:
+                indices[index] = len(indices) + 1
+
+    # % field indices are taken directly from the string, so they may leave gaps.
+    else:
+        matches = get_percent_fields(s)
+        for _, index in matches:
+            indices[index] = int(index)
+
+    return indices
+
+
+# Takes a string in {} or % format, and converts it to Java-compatible % format, taking into
+# account the given field indices generated by get_indices. Strings which are already in %
+# format will be checked for errors and returned unchanged.
+#
+# A string having fewer fields than the msgid isn't necessarily an error, e.g. some
+# translations omit a numeric field and use the equivalent of "a" or "one" instead. But if a
+# string references a field that isn't in the msgid, that usually causes a crash (e.g. #2358).
+def fix_format(s, indices):
+    INDEX_ERROR = "string {!r} uses field {!r}, which isn't in the msgid"
+
+    matches = get_brace_fields(s)
+    if matches:
+        # The string uses {} format, so any % signs cannot be fields, and should be escaped.
+        result = s.replace("%", "%%")
+
+        for field, index in matches:
+            try:
+                index_out = indices[index]
+            except KeyError:
+                if index not in args.ignore_unknown_keywords:
+                    raise CatalogError(INDEX_ERROR.format(s, index))
+            else:
+                result = result.replace(field,
+                                        "%s" if (len(indices) == 1) else f"%{index_out}$s",
+                                        1)
+        return result
+
+    else:
+        matches = get_percent_fields(s)
+        for field, index in matches:
+            # Only numeric fields allow a space flag. A space in any other field type is
+            # probably a typo (e.g. #1899) or an unescaped % sign.
+            if (" " in field) and (field[-1].lower() not in "doxefga"):
+                raise CatalogError(f"in string {s!r}, non-numeric field {field!r} contains a space")
+
+            if index not in indices:
+                raise CatalogError(INDEX_ERROR.format(s, index))
+
+        return s
+
+
+# Return a list of {} fields in a string. Each field is returned as a tuple of:
+#   * The entire field, including the braces.
+#   * The field keyword or index as a string, with implicit fields numbered from 0.
+def get_brace_fields(s):
+    # TODO: handle more complex syntax like "{x:.3f}".
+    matches = re.findall(r"(\{(\w+)?\})", s)
+    return fill_implicit(matches, 0)
+
+
+# Return a list of % fields in a string. Each field is returned as a tuple of:
+#   * The entire field, including the %.
+#   * The field index as a string, with implicit fields numbered from 1.
+def get_percent_fields(s):
+    matches = re.findall(r"(%(?:(\d+)\$)?.*?[a-zA-Z])", s.replace("%%", ""))
+    return fill_implicit(matches, 1)
+
+
+def fill_implicit(matches, start):
+    next = start
+
+    result = []
+    for i, (field, index) in enumerate(matches):
+        if not index:
+            index = str(next)
+            next += 1
+        result.append((field, index))
+    return result
 
 
 # The region with the most translations is output without a country code so it will act as
@@ -167,13 +278,16 @@ def region_order(item):
 
 def parse_args():
     ap = argparse.ArgumentParser()
-    ap.add_argument("--no-download", action="store_true")
+    ap.add_argument("--no-download", action="store_true", help="Don't run make_locale")
+    ap.add_argument("--ignore-unknown-keywords", metavar="KEYWORD", nargs="+", default=[],
+                    help="Keywords which can appear in a string without being in the msgid")
+    ap.add_argument("--out", metavar="DIR", type=abspath, required=True,
+                    help="Output resources directory")
     return ap.parse_args()
 
 
 def is_excluded(src_str):
-    return (re.search(r"^\W*$", src_str) or         # Empty or only punctuation.
-            src_str in ["Auto connect"])            # Clashes with "Auto-connect".
+    return bool(re.search(r"^\W*$", src_str))  # Empty or only punctuation.
 
 
 # Returns a dict {s: id} where each `s` is a string in `strings`, and `id` is a unique
@@ -205,13 +319,13 @@ def make_ids_inner(ids_in, ids_out):
     prev_counts = None
     while ids_in:
         counts = Counter(existing_ids)
-        counts.update([id[:max_words] for id in ids_in.values()])
+        counts.update([shorten_id(id, max_words) for id in ids_in.values()])
         if counts == prev_counts:
             raise DuplicateStringError()
 
         strings_done = []
         for s, id in ids_in.items():
-            short_id = id[:max_words]
+            short_id = shorten_id(id, max_words)
             if counts[short_id] == 1:
                 ids_out[s] = short_id
                 strings_done.append(s)
@@ -222,12 +336,28 @@ def make_ids_inner(ids_in, ids_out):
         max_words += 1
 
 
+# We still need to preserve empty words to avoid duplicate IDs. But we don't count them against
+# the word limit, otherwise we end up with IDs like "__1" or "_", the last of which isn't even
+# legal in Java 9.
+def shorten_id(id, max_words):
+    result = []
+    num_words = 0
+    for word in id:
+        result.append(word)
+        if word:
+            num_words += 1
+        if num_words == max_words:
+            break
+    return tuple(result)
+
+
 class DuplicateStringError(Exception):
     pass
 
 
 # Returns an identifier generated from every word in the given string.
 def str_to_id(s, *, lower, squash):
+    s_original = s
     s = s.replace("'", "")  # Combine contractions.
     if lower:
         s = s.lower()
@@ -244,17 +374,24 @@ def str_to_id(s, *, lower, squash):
     id = (re.sub(pattern, "_", s, flags=re.ASCII)  # Remove invalid characters.
           .lstrip(lstrip)
           .rstrip(rstrip))
+
+    if not id or not re.search(r"^[a-zA-Z_]", id):
+        if squash:
+            return str_to_id(s_original, lower=lower, squash=False)
+        else:
+            raise Exception(f"string {s_original!r} generated invalid identifier {id!r}")
+
     if id in KEYWORDS:
         id += "_"
     return id
 
 
-def write_xml(res_suffix, strings, ids):
+def write_xml(res_dir, res_suffix, strings, ids):
+    res_suffix = RENAMED_LANGUAGES.get(res_suffix, res_suffix)
     dir_name = "values" + ("-" + res_suffix if res_suffix else "")
     base_name = "strings.xml"
-    log("Generating {}/{}: ".format(dir_name, base_name), end="")
-
-    abs_dir_name = join(EC_ROOT, "android/app/src/main/res", dir_name)
+    log("{}/{}: ".format(dir_name, base_name), end="")
+    abs_dir_name = join(res_dir, dir_name)
     os.makedirs(abs_dir_name, exist_ok=True)
 
     timestamp = datetime.utcnow().isoformat()
@@ -265,7 +402,8 @@ def write_xml(res_suffix, strings, ids):
     with open(join(abs_dir_name, base_name), "w", encoding="UTF-8") as f:
         print('<?xml version="1.0" encoding="utf-8"?>', file=f)
         print('<!-- Generated by {} at {} -->'.format(SCRIPT_NAME, timestamp), file=f)
-        print('<!-- DO NOT EDIT: edit the source Python files instead. -->', file=f)
+        print('<!-- DO NOT EDIT this file directly. See "Strings" in android/README.md. -->',
+              file=f)
         print('<resources>', file=f)
         for id, tgt in output:
             if isinstance(tgt, dict):
@@ -289,7 +427,7 @@ XML_REPLACEMENTS = [
 
     # Android-specific syntax
     # (https://developer.android.com/guide/topics/resources/string-resource#escaping_quotes)
-    (re.compile(r"^([@?])"), r"\1"),
+    (re.compile(r"^([@?])"), r"\\\1"),
     ("'", r"\'"),
     ('"', r'\"'),
     ("\n", r"\n"),
